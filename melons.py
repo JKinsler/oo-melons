c18084ef3a1f5ac962c499524e5b594a36ee9f7a
Your Name <you@example.com>
2020-01-17 15:29:36 -0800

    Working code for class GovernmentMelonOrder

--- melons.py
+++ melons.py
@@ -1,7 +1,7 @@
 """Classes for melon orders."""
 
 
-class AbstractMelonOrder():
+class AbstractMelonOrder(object):
     """An abstract base clasee that other Melon Orders inherit from."""
 
     def __init__(self, species, qty):
@@ -17,6 +17,19 @@ class AbstractMelonOrder():
         self.shipped = True
 
 
+class GovernmentMelonOrder(AbstractMelonOrder):
+    """ """
+    order_type = "government"
+    tax = 0
+    # passed = False
+    passed_inspection = False
+
+    def mark_inspection(self, passed):
+        # if passed:
+        passed_inspection = passed
+        return passed_inspection
+
+
 class DomesticMelonOrder(AbstractMelonOrder):
     """A melon order within the USA."""
 
@@ -30,6 +43,10 @@ class DomesticMelonOrder(AbstractMelonOrder):
         """Calculate price, including tax."""
 
         base_price = 5
+
+        if self.species == "Christmas":
+            base_price *= 1.5
+
         total = (1 + self.tax) * self.qty * base_price
 
         return total
@@ -41,8 +58,11 @@ class InternationalMelonOrder(AbstractMelonOrder):
     order_type = "international"
     tax = 0.17
 
+    # requires 4 arguments
     def __init__(self, species, qty, country_code):
         """Initialize melon order attributes."""
+
+        #  taking species and qty from AbstractMelonOrder
         super().__init__(species, qty)
 
         self.country_code = country_code
@@ -55,7 +75,16 @@ class InternationalMelonOrder(AbstractMelonOrder):
     def get_total(self):
         """Calculate price, including tax."""
 
+        if self.qty < 10:
+            flat_fee = 3
+        else:
+            flat_fee = 0
+
         base_price = 5
-        total = (1 + self.tax) * self.qty * base_price
+
+        if self.species == "Christmas":
+            base_price *= 1.5
+
+        total = (1 + self.tax) * self.qty * base_price + flat_fee
 
         return total
